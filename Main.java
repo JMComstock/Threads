4bd75d86e6e728c1bf60f853aa38b15396a76674
Jason Comstock <71102533+JMComstock@users.noreply.github.com>
2021-09-21 15:57:49 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,7 +1,6 @@
 package com.company;
 
-import static com.company.ThreadColor.ANSI_GREEN;
-import static com.company.ThreadColor.ANSI_PURPLE;
+import static com.company.ThreadColor.*;
 
 public class Main {
 
@@ -9,6 +8,7 @@ public class Main {
         System.out.println(ANSI_PURPLE + "Hello from the main thread.");
 
         Thread anotherThread = new AnotherThread();
+        anotherThread.setName("== Another Thread ==");
         anotherThread.start();
 
         new Thread() {
@@ -17,8 +17,22 @@ public class Main {
             }
         }.start();
 
+        Thread myRunnableThread = new Thread (new MyRunnable() {
+            @Override
+            public void run() {
+                System.out.println(ANSI_RED + "Hello from the anonymous class's implementation of run().");
+                try {
+                    anotherThread.join();
+                    System.out.println(ANSI_RED + "AnotherThread terminated, or timed out, so I'm running again.");
+                } catch(InterruptedException e) {
+                    System.out.println(ANSI_RED + "I couldn't wait after all.  I was interrupted");
+                }
+            }
+        });
+
+        myRunnableThread.start();
+
         System.out.println(ANSI_PURPLE + "Hello again from the main thread.");
 
     }
-
 }
